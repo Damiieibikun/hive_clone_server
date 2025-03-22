faf7d4f88ff4a3a9340a3ad39e00249f5dc3a1be
Damilola Ibikunle <damiieibikun@gmail.com>
2025-03-22 16:58:18 +0100

    fixed a bug

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const connectDatabase = require('./config/databaseConfig');
 const app = express();
 const {config} = require('./config/config')
 const cors = require('cors');
-app.use(cors());
 const user_routes = require('./routes/userRoutes');
 const category_routes = require('./routes/categoryRoutes');
 const post_routes = require('./routes/postRoutes');
@@ -11,6 +10,15 @@ const comments_routes = require('./routes/commentsRoutes');
 const likes_route = require('./routes/likesRoutes')
 
 const path = require('path');
+app.use(cors({
+    origin: 'https://hive-clone.onrender.com', 
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+  }));
+  
+    app.options('*', cors());
+
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use(express.json());
